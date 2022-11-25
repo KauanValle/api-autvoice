9e18b37a5a320508659f362d95155ba56dadb325
Auto Logon <autologon@CSED.LAB>
2022-11-25 19:31:57 -0300

    model profissional

--- app/Models/Profissional.php
+++ app/Models/Profissional.php
@@ -11,6 +11,23 @@ class ProfissionalModel extends Model
 
     const TABLE = 'profissionais';
 
+    protected $table = self::TABLE;
 
+        const NOME = 'nome';
+        const DATA_NASCIMENTO = 'data_nascimento';
+        const CRM = 'crm';
+        const EMAIL = 'email';
+        const DADOS ='dados';
+        const SENHA ='senha';
+
+        protected $fillable = [
+            self::ID_ENDERECO,
+            self::CRM,
+            self::NOME,
+            self::DATA_NASCIMENTO,
+            self::DADOS,
+            self::SENHA,
+            self::EMAIL
+        ];
 
 }
